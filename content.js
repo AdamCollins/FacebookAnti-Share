d5bd243286a4201461b7ea1d5c10ab8f7cdddafb
Adam Collins <adamcollinssecure@gmail.com>
2017-04-21 11:32:16 -0700

    added options page and ability to hide other types of spam

--- content.js
+++ content.js
@@ -1,8 +1,38 @@
-console.log("Facebook anti-share loading...")
-removeShares();
-window.setInterval(removeShares,1000);
+var isEnabled;
+var removeShares;
+var removeSponsored;
+var removeCommentReplies;
+chrome.storage.sync.get(function(config){
 
-function removeShares(){
-	console.log("removing shares...");
-	$(".fcg:contains('shared')").parents().eq(11).remove();
+	isEnabled = config.isEnabled;
+	removeShares = config.hideShares;
+	removeSponsored = config.hideSponsored;
+	removeCommentReplies = config.hideCommentReplies;
+	console.log("isEnabled:"+isEnabled);
+	console.log("removeShares:"+removeShares);
+	console.log("removeSponsored:"+removeSponsored);
+	console.log("hideCommentReplies:"+removeCommentReplies);
+
+
+});
+console.log("Facebook anti-share loading...");
+removeSpam();
+window.setInterval(removeSpam,1000);
+
+function removeSpam(){
+	if(isEnabled)
+	{
+		console.log("removing shares...");
+		//removes shared posts
+		if(removeShares)
+			$(".fcg:contains('shared')").parents().eq(11).remove();
+
+		//removes comment replies
+		if(removeCommentReplies)
+			$(".fcg:contains('comment')").parents().eq(11).remove();
+
+		//removes Sponsored posts
+		if(removeSponsored)
+			$("._3e_2._m8c:contains('Sponsored')").parents().eq(11).remove();
+	}
 }
---
+++ options.js
@@ -0,0 +1,41 @@
+// Saves options to chrome.storage
+function save_options() {
+  console.log("saved");
+  var on = document.getElementById("isEnabled").checked;
+  var hideShares = document.getElementById("hideShares").checked;
+  var hideSponsored = document.getElementById("hideSponsoredPosts").checked;
+  var hideCommentReplies = document.getElementById("hideCommentReplies").checked;
+  console.log("hide cr:"+hideCommentReplies);
+    chrome.storage.sync.set({
+    isEnabled: on,
+    hideShares: hideShares,
+    hideCommentReplies: hideCommentReplies,
+    hideSponsored: hideSponsored
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    isEnabled: true,
+    hideShares: true,
+    hideCommentReplies: false,
+    hideSponsored: true
+  }, function(items) {
+    document.getElementById('isEnabled').checked = items.isEnabled;
+    document.getElementById('hideShares').checked = items.hideShares;
+    document.getElementById('hideSponsoredPosts').checked = items.hideSponsored;
+     document.getElementById('hideCommentReplies').checked = items.hideCommentReplies;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
